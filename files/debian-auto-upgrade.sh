11b0740d8f01ab2db4c8bbd76b91cdc971606381
James Cherti <60946298+jamescherti@users.noreply.github.com>
2025-07-06 15:15:17 -0400

    Misc enhancements

---
+++ files/debian-auto-upgrade.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -euf -o pipefail
+
+# shellcheck disable=SC2317
+error_handler() {
+  local errno="$?"
+  echo "Error: ${BASH_SOURCE[1]}:${BASH_LINENO[0]}" \
+    "(${BASH_COMMAND} exited with status $errno)" >&2
+  exit "${errno}"
+}
+
+wait_for_successful_apt_update() {
+  # 720 minutes = 12 hours
+  local i
+  for ((i = 0; i < 720; i++)); do
+    if apt-get update >/dev/null 2>&1; then
+      break
+    else
+      echo "No Internet connection. Wait 1 minute..."
+      sleep 60
+    fi
+  done
+}
+
+main() {
+  trap "error_handler" ERR
+  set -o errtrace
+
+  export DEBIAN_FRONTEND=noninteractive
+
+  wait_for_successful_apt_update
+
+  apt-get update
+  apt-get upgrade -y
+  apt-get autoremove --purge -y
+  apt-get clean -y
+}
+
+main "$@"
